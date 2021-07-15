234452b3e2e2bb2a360d1eddf35decdb67e2d4c5
vishnu vardhan <vishnu vardhan@KORATALA>
2021-07-15 22:29:17 +0530

    First Commit

---
+++ canarydetect/src/canarydetect/Class1.java
@@ -0,0 +1,45 @@
+package canarydetect;
+
+import java.io.FileInputStream;
+
+import java.io.IOException;
+import java.util.Properties;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.remote.RemoteWebElement;
+
+
+
+public class Class1 {
+
+	public static void main(String[] args) throws IOException, InterruptedException {
+		FileInputStream f1=new FileInputStream("D:\\selenium\\canarydetect\\src\\canarydetect\\or1");
+		Properties p1=new Properties();
+		p1.load(f1);
+		
+		System.setProperty("webdriver.chrome.driver", System.getProperty("user.dir")+"\\drivers\\chromedriver.exe");
+		WebDriver vk=new ChromeDriver();
+		vk.navigate().to(p1.getProperty("url1"));
+		vk.manage().window().maximize();
+		vk.findElement(By.xpath(p1.getProperty("contact")));
+		vk.findElement(By.xpath("//body/div[@id='siteWrapper']/main[@id='page']/article[@id='sections']/section[2]/div[2]/div[1]/div[1]/div[1]/div[1]/div[1]/div[3]/div[1]/div[1]/div[1]/div[1]/form[1]/div[1]/fieldset[1]/div[1]/label[1]/input[1]")).sendKeys("Vishnu");
+		vk.findElement(By.xpath("//body/div[@id='siteWrapper']/main[@id='page']/article[@id='sections']/section[2]/div[2]/div[1]/div[1]/div[1]/div[1]/div[1]/div[3]/div[1]/div[1]/div[1]/div[1]/form[1]/div[1]/fieldset[1]/div[2]/label[1]/input[1]")).sendKeys("Koratala");
+		vk.findElement(By.xpath("//body[1]/div[1]/main[1]/article[1]/section[2]/div[2]/div[1]/div[1]/div[1]/div[1]/div[1]/div[3]/div[1]/div[1]/div[1]/div[1]/form[1]/div[1]/div[1]/input[1]")).sendKeys("vkoratala@gmail.com");
+		vk.findElement(By.xpath("//body[1]/div[1]/main[1]/article[1]/section[2]/div[2]/div[1]/div[1]/div[1]/div[1]/div[1]/div[3]/div[1]/div[1]/div[1]/div[1]/form[1]/div[1]/div[2]/input[1]")).sendKeys("Test");
+		vk.findElement(By.xpath("//body[1]/div[1]/main[1]/article[1]/section[2]/div[2]/div[1]/div[1]/div[1]/div[1]/div[1]/div[3]/div[1]/div[1]/div[1]/div[1]/form[1]/div[1]/div[3]/textarea[1]")).sendKeys("test");
+		JavascriptExecutor js = (JavascriptExecutor) vk;
+		js.executeScript("window.scrollBy(697,745)", "");
+		Thread.sleep(3500);
+		vk.findElement(By.xpath("//input[@name='radio-4dd8c673-4231-4b90-9595-2aa03f5d7330-field'][@value='Yes']")).click();
+		vk.findElement(By.xpath("//body[1]/div[1]/main[1]/article[1]/section[2]/div[2]/div[1]/div[1]/div[1]/div[1]/div[1]/div[3]/div[1]/div[1]/div[1]/div[1]/form[1]/div[2]/input[1]")).submit();
+		Thread.sleep(3500);
+		vk.close();		
+		
+
+	}
+
+}
